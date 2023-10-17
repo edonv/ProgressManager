2673f183962f0c5bd28d66288e1b808e021dc276
Edon <edon@valdman.works>
2023-10-17 17:41:56 -0500

    Update ProgressManager.swift
    Added the base of `ProgressManager`.

--- Sources/ProgressManager/ProgressManager.swift
+++ Sources/ProgressManager/ProgressManager.swift
@@ -1,2 +1,28 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  ProgressManager.swift
+//
+//
+//  Created by Edon Valdman on 10/17/23.
+//
+
+import Foundation
+import Combine
+
+public final class ProgressManager<ChildTaskKey: Hashable> {
+    public let parent: Progress
+    public let childTasks: [ChildTaskKey: Progress]
+    
+    public init(childTaskUnitCounts: [ChildTaskKey: Int64], childTaskUnitCountsInParent: [ChildTaskKey: Int64]? = nil) {
+        let totalParentUnitCount: Int64 = childTaskUnitCounts.reduce(into: 0) { updatingUnitCount, countKVP in
+            updatingUnitCount += childTaskUnitCountsInParent?[countKVP.key] ?? countKVP.value
+        }
+        
+        self.parent = Progress(totalUnitCount: totalParentUnitCount)
+        
+        self.childTasks = childTaskUnitCounts.reduce(into: [:]) { [parent] dict, countKVP in
+            dict[countKVP.key] = Progress(totalUnitCount: countKVP.value,
+                                          parent: parent,
+                                          pendingUnitCount: childTaskUnitCountsInParent?[countKVP.key] ?? countKVP.value)
+        }
+    }
+}
