dd80d3875ae375e90dc7dedc53b9fde7d2b8c400
Edon <edon@valdman.works>
2023-10-18 11:42:31 -0500

    Update ProgressManager.swift
    Added explicit `return`s, as it can only be implicit in Swift 5.9+.

--- Sources/ProgressManager/ProgressManager.swift
+++ Sources/ProgressManager/ProgressManager.swift
@@ -113,11 +113,11 @@ extension ProgressManager {
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func fractionCompletedPublisher(forChild childKey: ChildTaskKey) -> AnyPublisher<Double?, Never> {
         if let child = childTasks[childKey] {
-            child.publisher(for: \.fractionCompleted)
+            return child.publisher(for: \.fractionCompleted)
                 .map { $0 as Double? }
                 .eraseToAnyPublisher()
         } else {
-            Just(nil)
+            return Just(nil)
                 .eraseToAnyPublisher()
         }
     }
@@ -129,11 +129,11 @@ extension ProgressManager {
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func totalUnitCountPublisher(forChild childKey: ChildTaskKey) -> AnyPublisher<Int64?, Never> {
         if let child = childTasks[childKey] {
-            child.publisher(for: \.totalUnitCount)
+            return child.publisher(for: \.totalUnitCount)
                 .map { $0 as Int64? }
                 .eraseToAnyPublisher()
         } else {
-            Just(nil)
+            return Just(nil)
                 .eraseToAnyPublisher()
         }
     }
@@ -146,11 +146,11 @@ extension ProgressManager {
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func completedUnitCountPublisher(forChild childKey: ChildTaskKey) -> AnyPublisher<Int64?, Never> {
         if let child = childTasks[childKey] {
-            child.publisher(for: \.completedUnitCount)
+            return child.publisher(for: \.completedUnitCount)
                 .map { $0 as Int64? }
                 .eraseToAnyPublisher()
         } else {
-            Just(nil)
+            return Just(nil)
                 .eraseToAnyPublisher()
         }
     }
