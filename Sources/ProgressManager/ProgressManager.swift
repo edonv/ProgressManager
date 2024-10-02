f493a1b317eb8f4923cbd93b41cdc1110d157030
Edon <edon@valdman.works>
2024-10-02 10:16:44 -0500

    Update ProgressManager.swift
    Fixed mistakes in documentation.

--- Sources/ProgressManager/ProgressManager.swift
+++ Sources/ProgressManager/ProgressManager.swift
@@ -61,7 +61,7 @@ extension ProgressManager {
     /// Sets the [`completedUnitCount`](https://developer.apple.com/documentation/foundation/progress/1407934-completedunitcount) property of the child task with the associated key to the provided value.
     /// - Parameters:
     ///   - completedUnitCount: The new count of completed units for the child task associated with the provided key.
-    ///   - key: The key of a child task to update.
+    ///   - childTask: The key of a child task to update.
     public func setCompletedUnitCount(
         _ completedUnitCount: Int64,
         forChildTask childTask: ChildTask
@@ -72,7 +72,7 @@ extension ProgressManager {
     /// Updates the [`completedUnitCount`](https://developer.apple.com/documentation/foundation/progress/1407934-completedunitcount) property of the child task with the associated key by adding the provided value to the current value.
     /// - Parameters:
     ///   - newlyCompletedUnitCountToAdd: A count of completed units to add to the current value of the child task associated with the provided key.
-    ///   - key: The key of a child task to update.
+    ///   - childTask: The key of a child task to update.
     public func addToCompletedUnitCount(
         _ newlyCompletedUnitCountToAdd: Int64,
         forChildTask childTask: ChildTask
@@ -82,9 +82,8 @@ extension ProgressManager {
     
     /// Updates the [`completedUnitCount`](https://developer.apple.com/documentation/foundation/progress/1407934-completedunitcount) property of the child task with the associated key by calling the provided closure.
     /// - Parameters:
-    ///   - key: The key of a child task to update.
-    ///   - updateClosure: A closure that should return an updated current count of completed units.
-    ///   - currentValue: The current count of completed units.
+    ///   - childTask: The key of a child task to update.
+    ///   - updateClosure: A closure, provided with the current count of completed units, that should return an updated current count of completed units.
     public func updateCompletedUnitCount(
         forChildTask childTask: ChildTask,
         updateClosure: (_ currentValue: Int64) -> Int64
@@ -233,7 +232,7 @@ extension ProgressManager {
     ///
     /// If there isn't a child task matching the key, the returned `Publisher` will return `nil` then finish.
     /// - Note: This publisher updates continuously as work progresses for the child task.
-    /// - Parameter childKey: The key of a child task from which to get a publisher (if such a child task exists).
+    /// - Parameter childTask: The key of a child task from which to get a publisher (if such a child task exists).
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func fractionCompletedPublisher(forChildTask childTask: ChildTask) -> AnyPublisher<Double?, Never> {
         if let child = childTasks[childTask] {
@@ -249,7 +248,7 @@ extension ProgressManager {
     /// Publishes changes to [`totalUnitCount`](https://developer.apple.com/documentation/foundation/progress/1410940-totalunitcount) on the child task (from ``childTasks``) associated with `childKey` (if there is one).
     ///
     /// If there isn't a child task matching the key, the returned `Publisher` will return `nil` then finish.
-    /// - Parameter childKey: The key of a child task from which to get a publisher (if such a child task exists).
+    /// - Parameter childTask: The key of a child task from which to get a publisher (if such a child task exists).
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func totalUnitCountPublisher(forChildTask childTask: ChildTask) -> AnyPublisher<Int64?, Never> {
         if let child = childTasks[childTask] {
@@ -266,7 +265,7 @@ extension ProgressManager {
     ///
     /// If there isn't a child task matching the key, the returned `Publisher` will return `nil` then finish.
     /// - Note: This publisher only updates when the child task is `100%` complete.
-    /// - Parameter childKey: The key of a child task from which to get a publisher (if such a child task exists).
+    /// - Parameter childTask: The key of a child task from which to get a publisher (if such a child task exists).
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     public func completedUnitCountPublisher(forChildTask childTask: ChildTask) -> AnyPublisher<Int64?, Never> {
         if let child = childTasks[childTask] {
